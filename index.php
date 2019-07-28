fb41d0c868fd62ee02e39ab87133c91f18acdd64
SERDAR CİMEN <serdarrasimcimen@gmail.com>
2019-07-28 19:45:02 +0300

    a

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Bulmaca Sözlüğü</title>
+    <title>Eş=Anlam</title>
   </head>
   <body>
 
@@ -16,7 +16,7 @@
     <div class="container">
 
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">BulmacaSözlüğü</a>
+        <a class="navbar-brand" href="index.php">Eş=Anlam</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
